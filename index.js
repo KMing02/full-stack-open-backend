3bcae1cffe7aed47c638269d7b8dede6b7f0150a
KMing02 <676906733@qq.com>
2023-08-24 11:12:46 +0800

    Deploying the PhoneBook App locally
    Successfully run the app locally

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const morgan = require('morgan')
 
-app.use(express.static('dist'))
+app.use(cors)
+app.use(express.static('build'))
 app.use(express.json())
 morgan.token('body', req => {
     return JSON.stringify(req.body)
-  })
+})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
