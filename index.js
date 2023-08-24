c35c9f1f072225d96f6eb138df442ab7d4a6c892
KMing02 <676906733@qq.com>
2023-08-24 12:11:19 +0800

    implement cors middleware

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 
-app.use(cors)
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 morgan.token('body', req => {
