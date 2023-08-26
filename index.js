a30c6bfb5c43c3e943223c4831222833fb84069d
KMing02 <676906733@qq.com>
2023-08-26 15:47:45 +0800

    add .env
    add the url of database to environment variable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
+const Entry = require('./models/persons')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -22,31 +23,11 @@ morgan.token('body', req => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-    response.send(persons)
+  Entry.find({}).then(entries => {
+    response.json(entries)
+  })
 })
 
 app.get('/info', (request, response) => {
