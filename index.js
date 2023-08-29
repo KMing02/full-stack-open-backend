63b7def33ca92f0b5f17e8d65c15228d751162d9
KMing02 <676906733@qq.com>
2023-08-29 13:21:23 +0800

    configure eslint

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const morgan = require('morgan')
 const Phone = require('./models/persons')
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
+	next()
 }
 
 app.use(cors())
@@ -19,98 +19,98 @@ app.use(express.json())
 app.use(requestLogger)
 
 morgan.token('body', req => {
-    return JSON.stringify(req.body)
+	return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 app.get('/api/persons', (request, response) => {
-  Phone.find({}).then(entries => {
-    response.json(entries)
-  })
+	Phone.find({}).then(entries => {
+		response.json(entries)
+	})
 })
 
 app.get('/info', async (request, response) => {
-  const count = await Phone.countDocuments()
-  response.send(`<p>Phonebook has info for ${count} people</p><p>${new Date}</p>`)
+	const count = await Phone.countDocuments()
+	response.send(`<p>Phonebook has info for ${count} people</p><p>${new Date}</p>`)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  const id = request.params.id
-  Phone.findById(id).then(entry => {
-      if (entry) {
-        response.json(entry)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-  })
+	const id = request.params.id
+	Phone.findById(id).then(entry => {
+		if (entry) {
+			response.json(entry)
+		} else {
+			response.status(404).end()
+		}
+	})
+		.catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Phone.findByIdAndRemove(request.params.id)
-    .then(result => {
-    response.status(204).end()
-  })
-    .catch(error => next(error))
+	Phone.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
   
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+	const body = request.body
   
-    if (!body.number || !body.name) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
+	if (!body.number || !body.name) {
+		return response.status(400).json({ 
+			error: 'content missing' 
+		})
+	}
   
-    const person = new Phone({
-      name: body.name,
-      number: body.number,
-    })
-    person.save()
-    .then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
-  })
+	const person = new Phone({
+		name: body.name,
+		number: body.number,
+	})
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+	const body = request.body
 
-  const entry = {
-    name: body.name,
-    number: body.number
-  }
+	const entry = {
+		name: body.name,
+		number: body.number
+	}
 
-  Phone.findByIdAndUpdate(
-    request.params.id, 
-    entry, 
-    {new: true, runValidators: true, context: 'query'})
-  .then(updatedEntry => {
-    response.json(updatedEntry)
-  })
-  .catch(error => next(error))
+	Phone.findByIdAndUpdate(
+		request.params.id, 
+		entry, 
+		{new: true, runValidators: true, context: 'query'})
+		.then(updatedEntry => {
+			response.json(updatedEntry)
+		})
+		.catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
 
-  next(error)
+	next(error)
 }
 
 // this has to be the last loaded middleware.
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
--- models/persons.js
+++ models/persons.js
@@ -7,43 +7,43 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+	.then(result => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const validators = [
-    {validator : function(v) {return(v.includes('-'))}, message: 'the number should contain -'},
-    {validator : 
+	{validator : function(v) {return(v.includes('-'))}, message: 'the number should contain -'},
+	{validator : 
         function(v) {return((v.split('-')[0].length === 2) || (v.split('-')[0].length === 3))}, 
-        message: 'the left part of - should consist of 2 or 3 numbers'},
-    {validator:
+	message: 'the left part of - should consist of 2 or 3 numbers'},
+	{validator:
         function(v) {return((!(isNaN(v.split('-')[1]))) && (!(v.split('-').length > 2)))},
-        message: 'the right part should be a valid number'
-    }
+	message: 'the right part should be a valid number'
+	}
 ]
 
 const entrySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minLength: 9,
-    validate: validators
-    }
+	name: {
+		type: String,
+		minLength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		minLength: 9,
+		validate: validators
+	}
 })
 
 entrySchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 
